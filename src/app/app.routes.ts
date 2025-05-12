f5a8120a71ba3617c28fd4bb89155725d3a4bb4f
seinosuke14 <diegobascur9@gmail.com>
2025-05-12 13:37:54 -0400

    Firt-Create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'header', component:HeaderComponent},
+  {path: 'footer', component:FooterComponent}
+
+];
